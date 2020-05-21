bce8b961b0b7e87850a62f0785e6fecfb0987156
Mick Carter <cartemic@oregonstate.edu>
2020-05-21 08:46:37 -0700

    Close multiprocessing pool in sd calculation

--- pypbomb/sd.py
+++ pypbomb/sd.py
@@ -394,6 +394,7 @@ class Detonation:
             max_density_ratio = adjusted_density_ratio * (1 + 0.001)
             counter += 1
 
+        pool.close()
         cj_speed = a * adjusted_density_ratio**2 + \
             b * adjusted_density_ratio + c
 
